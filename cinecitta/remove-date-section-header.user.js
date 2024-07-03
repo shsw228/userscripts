3200e1dbe7194d380e1d8d1d9ef603405459f1aa
Tkng <13506491+shsw228@users.noreply.github.com>
2024-07-03 22:49:18 +0900

    fix deprecated API issue on iOS

--- cinecitta/remove-date-section-header.user.js
+++ cinecitta/remove-date-section-header.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         [cinecitta]いらない日付表示を削除する
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  なぜ3作品ごとに日付を表示するのか
 // @author       shsw228
 // @match        https://cinecitta.co.jp/*
 // @match        https://members.cinecitta.jp/Cspc/usr/login.do
 // @homepage    https://github.com/shsw228/userscripts/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=cinecitta.co.jp
-// @grant        GM_addStyle
+// @grant        GM.addStyle
 // @license     MIT License
 // ==/UserScript==
 
@@ -16,5 +16,5 @@ const css = `
 .between {
   display: none;
 }`;
-GM_addStyle(css);
+GM.addStyle(css);
 
